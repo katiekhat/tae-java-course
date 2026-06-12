cdd292167a6403998127e479ace980a66112a42f
Ketevan Khatiashvili <kkhatiashvili@credo.ge>
2026-06-12 13:12:20 +0400

    [Ketevan K.] HW8 - Age Validator

---
+++ src/main/java/homework8/InvalidAgeException.java
@@ -0,0 +1,9 @@
+/**@author <Ketevan Khatiashvili>*/
+package homework8;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message){
+        super(message);
+    }
+
+}
